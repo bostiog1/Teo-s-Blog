8610d827ebb73d04d5d5fa72e0aae324a3b5c221
Teo <teo.bostiog@yahoo.com>
2024-08-27 03:54:42 +0300

    feat: Added pagination for the posts

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,9 +1,55 @@
-import React from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
 import Post from "./Post";
-import { reorderPosts } from "../redux/postSlice"; // Import the action to reorder posts
 
+const POSTS_PER_PAGE = 4; // Number of posts per page
+
+const PostList = () => {
+  const posts = useSelector((state) => state.posts.filteredPosts); // Use filteredPosts
+  const [currentPage, setCurrentPage] = useState(1); // Track the current page
+
+  // Calculate the range of posts to display based on the current page
+  const startIndex = (currentPage - 1) * POSTS_PER_PAGE;
+  const currentPosts = posts.slice(startIndex, startIndex + POSTS_PER_PAGE); // Get posts for the current page
+
+  // Calculate the total number of pages
+  const totalPages = Math.ceil(posts.length / POSTS_PER_PAGE);
+
+  // Handle page change
+  const goToPage = (page) => {
+    setCurrentPage(page);
+  };
+
+  return (
+    <div>
+      {/* Display current posts */}
+      {currentPosts.map((post) => (
+        <Post key={post.id} post={post} />
+      ))}
+
+      {/* Pagination Controls */}
+      <div className="mt-4 flex justify-center">
+        {Array.from({ length: totalPages }, (_, i) => i + 1).map((page) => (
+          <button
+            key={page}
+            onClick={() => goToPage(page)}
+            className={`mx-1 px-3 py-1 border rounded ${
+              page === currentPage
+                ? "bg-blue-500 text-white dark:bg-blue-700 dark:text-white"
+                : "bg-white text-black dark:bg-gray-800 dark:text-gray-300"
+            } hover:bg-gray-200 dark:hover:bg-gray-700 transition`}
+          >
+            {page}
+          </button>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default PostList;
+
+/*
 const PostList = () => {
   const posts = useSelector((state) => state.posts.filteredPosts); // Use filteredPosts
   const dispatch = useDispatch();
@@ -49,3 +95,5 @@ const PostList = () => {
 };
 
 export default PostList;
+
+*/
